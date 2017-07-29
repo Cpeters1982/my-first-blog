fe867d32cdb44f5a7a0a852476c9ea4ba78c35d0
Chris Peters <peterschristopherryan@gmail.com>
2017-07-29 12:32:10 -0700

    Added some views, added some templates, extended the base

--- apps/tut_app/views.py
+++ apps/tut_app/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 import datetime
@@ -10,3 +10,7 @@ import datetime
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'tut_app/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'tut_app/post_detail.html', {'post': post})
